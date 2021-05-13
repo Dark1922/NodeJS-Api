bf15b417b8f2803341c235a2f473f8f91cb5f130
dark1922 <jvdematos004@gmail.com>
2021-05-13 16:16:20 -0300

    addCustomerIdToOrders.ts

--- api-vendas/src/shared/typeorm/migrations/1620929022090-addCustomerIdToOrders.ts
+++ api-vendas/src/shared/typeorm/migrations/1620929022090-addCustomerIdToOrders.ts
@@ -1,11 +1,33 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, TableColumn, TableForeignKey} from "typeorm";
 
 export class addCustomerIdToOrders1620929022090 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+          'orders', //nome da tabela relacionada
+          new TableColumn({
+             name: 'customer_id',
+             type: 'uuid',
+             isNullable: true,
+          }),
+        );
+
+        await queryRunner.createForeignKey(
+            'orders',
+            new TableForeignKey({
+              name: 'OrdersCustomer',
+              columnNames: ['customer_id'],
+              referencedTableName: 'customers',
+              referencedColumnNames: ['id'],
+              onDelete: 'SET NULL',
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropForeignKey('orders', 'OrdersCustomer');
+      await queryRunner.dropColumn('orders', 'customer_id');
+
     }
 
 }
