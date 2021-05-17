f05909a6338b5b10c60f3771523051e2237643e5
dark1922 <jvdematos004@gmail.com>
2021-05-17 18:24:02 -0300

    feat: docker redis host 8001, config key value

--- api-vendas/src/modules/products/services/ListProductService.ts
+++ api-vendas/src/modules/products/services/ListProductService.ts
@@ -1,12 +1,18 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
+    //instancia do redischace
+    const redisCache = new RedisCache();
+
     const products = productsRepository.find();
+
+    await redisCache.save('test', 'teste')//chave e valor como teste
 
     return products;
   }
--- api-vendas/src/shared/cache/RedisCache.ts
+++ api-vendas/src/shared/cache/RedisCache.ts
@@ -4,12 +4,16 @@ import cacheConfig from '@config/cache';
 export default class  RedisCache {
   private client : RedisClient; //atráves desse client vamos estar manipulando
 
-  constructor() {
+  constructor() {//chamando a configuração e instanciando no Redis
     this.client = new Redis(cacheConfig.config.redis) //da pasta config cache
   }
   public async save(key: string, value: any): Promise<void> {
-    console.log(key, value);
+   //vamos pegar nossa instancia que estáno client
+    await this.client.set(key, JSON.stringify(value));//transforma numa string
+
+
   }
+
   //recover é recuperar um dado ai passa a chave que é string
   //public async recover<T>(key: string): Promise<T | undefined> {
     //<T> que eles são do mesmo tipo
